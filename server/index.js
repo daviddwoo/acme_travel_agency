92d6eea10f5447eced10f840199443a749aeb988
David <dwoo1016@gmail.com>
2022-01-07 23:11:54 -0500

    added cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 const { models: {Client, Trip, Place}, syncAndSeed } = require('./db')
 
 app.use('/dist', express.static(path.join(__dirname, '..', 'dist')));
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '..', 'src', 'index.html')));
 
